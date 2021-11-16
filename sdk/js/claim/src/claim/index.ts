127556c9ee509aa56fabaa333a60fdf270954cc2
K Rhoda <kelsey.rhoda@spruceid.com>
2021-11-16 12:13:13 -0700

    Creates TS type for VC and initial VC for Discord, brings in DIDKit for usage for VC completion

--- sdk/js/claim/src/claim/index.ts
+++ sdk/js/claim/src/claim/index.ts
@@ -1,15 +1,87 @@
-// TODO: FLESH OUT
 export type Claim = {
-    [index: string]: object | string,
+    '@context': string | object | Array<string | object>,
+    id?: string,
+    type: string | Array<string>,
+    credentialSubject: {
+        id?: string,
+        [index: string]: any,
+    },
+    issuer?: string | {
+        id: string,
+        [index: string]: any,
+    },
+    issuanceDate?: string,
+    proof?: ClaimProof | Array<ClaimProof>
+    expirationDate?: string,
+    credentialStatus?: {
+        id: string,
+        type: string,
+        [index: string]: any,
+    },
+    termsOfUse?: Array<{
+        id?: string,
+        type: string,
+        [index: string]: any,
+    }>,
+    evidence?: {
+        id?: string,
+        type: Array<string>,
+        [index: string]: any,
+    } | Array<{
+        id?: string,
+        type: Array<string>,
+        [index: string]: any,
+    }>,
+    credentialSchema?: {
+        id: string,
+        type: string,
+        [index: string]: any,
+    } | Array<{
+        id: string,
+        type: string,
+        [index: string]: any,
+    }>
+    refreshService?: {
+        id: string,
+        type: string,
+        [index: string]: any,
+    } | Array<{
+        id: string,
+        type: string,
+        [index: string]: any,
+    }>
+
+    [index: string]: any
 }
+
+
+export type ClaimProof = {
+    '@context': string | object | Array<string | object>,
+    type: string,
+    proofPurpose?: 'assertionMethod' 
+    | 'authentication'
+    | 'keyAgreement'
+    | 'contractAgreement'
+    | 'capabilityInvocation'
+    | 'capabilityDelegation',
+    proofValue?: string,
+    challenge?: string,
+    creator?: string,
+    verificationMethod?: string,
+    created?: string,
+    domain?: string,
+    nonce?: string,
+    jws?: string,
+    [index: string]: any
+};
 
 // Generic data structure representing the needed information to make a claim 
 // assumes a signerId (public key in practice)
 // and a signed and unsigned version of the message.
 // Other information for specific claim making can be passed in the message type param.
 export interface SignedMessage<Message> {
+    credentialSubjectId: string,
     message: Message,
-    signerId: string,
     signed: string,
     unsigned: string
 }
@@ -38,4 +110,5 @@ export function newClaimMaker<Message>(opts: ClaimMakerOpts<Message>): ClaimMake
 // Should track did:pkh:n.
 // Break into own file?
 export type SignerType = 'eth' | 'tz';
+
 
--- sdk/js/claim/src/create/public_witness/index.ts
+++ sdk/js/claim/src/create/public_witness/index.ts
@@ -1,18 +1,15 @@
 import type {
     Claim, 
-    // ClaimMaker, 
-    // SignedMessage, 
-    // SignedToClaim
+    SignedMessage, 
 } from '../../claim/index';
+
 import {SignerType} from '../../claim/index';
 
+import {v4 as uuidV4} from 'uuid';
+
 // Used to typecheck up to date with imported Claim types.
-// Webpack should remove it, or at least could be configured to.
-const exhaustiveCheck = (arg: never) => {
-  // Forces the type checker to complain if you've missed a sum type.
-  // See https://dev.to/babak/exhaustive-type-checking-with-typescript-4l3f
-  console.error(`Impossible value found: ${arg}`);
-};
+// See https://dev.to/babak/exhaustive-type-checking-with-typescript-4l3f
+const exhaustiveCheck = (arg: never) => arg;
 
 // Types of public witness supported in default Rebase settings.
 export type PublicWitnessType = 'discord' | 'twitter';
@@ -22,9 +19,10 @@ export type PublicWitnessType = 'discord' | 'twitter';
 // a signerId (a public key)
 // a type to case switch on.
 export interface PublicWitnessInfo {
-    type: PublicWitnessType,
     posterId: string,
+    posterType: PublicWitnessType,
     signerId: string,
+    signerType: SignerType,
 }
 
 // String-creation functions shared with the rebase-issuer.
@@ -81,22 +79,57 @@ export function signerDisplay(signerType: SignerType): string {
 }
 
 // Now put it all togethere.
-export const toUnsignedMessage = (info: PublicWitnessInfo, signerType: SignerType): string => {
+export function toUnsignedMessage(info: PublicWitnessInfo, signerType: SignerType): string {
     let {posterId, signerId} = info;
-    return `I attest ${posterPrefix(info.type)}${posterId} is linked to ${signerDisplay(signerType)} ${signerId}${seperator(info.type)}`;
+    return `I attest ${posterPrefix(info.posterType)}${posterId} is linked to ${signerDisplay(signerType)} ${signerId}${seperator(info.posterType)}`;
 }
 
 // Discord Specific
 export interface DiscordInfo extends PublicWitnessInfo {
     type: 'discord',
-    signerType: SignerType,
-    postId: string,
+    messageId: string,
+    channelId: string
 }
 
-export async function makeDiscordClaim(info: DiscordInfo): Promise<Claim> {
-    // TODO: Implement.
-    console.log(info);
-    return {}
+export async function toDiscordClaim(signedMessage: SignedMessage<DiscordInfo>, issuer: string): Promise<Claim> {
+    return {
+        "@context": [
+          "https://www.w3.org/2018/credentials/v1",
+          {
+              "sameAs": "http://schema.org/sameAs",
+              "DiscordVerification": "https://w3id.org/rebase/DiscordVerification",
+              "DiscordVerificationContents": {
+                  "@id": "https://w3id.org/rebase/DiscordVerificationContents",
+                  "@context": {
+                      "@version": 1.1,
+                      "@protected": true,
+                      "handle": "https://schema.org/text",
+                      "timestamp": {
+                          "@id": "https://schema.org/datetime",
+                          "@type": "http://www.w3.org/2001/XMLSchema#dateTime"
+                      },
+                      "channelId": "https://schema.org/text",
+                      "messageId": "https://schema.org/text"
+                  }
+              }
+          }
+        ],
+        credentialSubject: {
+            id: signedMessage.credentialSubjectId,
+            sameAs: `urn:discord:${signedMessage.message.posterId}`
+        },
+        evidence: {
+            channelId: signedMessage.message.channelId,
+            handle: signedMessage.message.posterId,
+            messageId: signedMessage.message.messageId,
+            timestamp: new Date().toISOString(),
+            type: ['DiscordVerificationContents']
+        },
+        id: `urn:uuid:${uuidV4()}`,
+        issuer,
+        // TODO: MAKE PROOF BASED ON SIGNER TYPE:
+        type: ['VerifiableCredential', 'DiscordVerification']
+    };
 }
 
 // Twitter Specific
