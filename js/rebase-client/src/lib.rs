c0f35c97ec7d24fcbbf60b0c57c31561c2acfab2
Jacob <jacobbward0@gmail.com>
2022-07-28 11:01:03 +0100

    Convert js functions to non-consuming methods.

--- js/rebase-client/src/lib.rs
+++ js/rebase-client/src/lib.rs
@@ -6,6 +6,7 @@ use rebase_witness_sdk::{
 };
 // use serde_json::from_str;
 use js_sys::Promise;
+use std::sync::Arc;
 use url::Url;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::future_to_promise;
@@ -29,13 +30,17 @@ macro_rules! jserr {
 
 #[wasm_bindgen]
 pub struct Client {
-    client: RebaseClient,
+    client: Arc<RebaseClient>,
 }
 
 #[wasm_bindgen]
 impl Client {
     #[wasm_bindgen(constructor)]
-    pub fn new(statement: String, jwt: Option<String>, ld: Option<String>) -> Result<Client, String> {
+    pub fn new(
+        statement: String,
+        jwt: Option<String>,
+        ld: Option<String>,
+    ) -> Result<Client, String> {
         let jwt: Option<Url> = match jwt {
             Some(s) => Some(Url::parse(&s).map_err(|e| e.to_string())?),
             None => None,
@@ -48,35 +53,39 @@ impl Client {
 
         let statement = Url::parse(&statement).map_err(|e| e.to_string())?;
         Ok(Client {
-            client: RebaseClient::new(Endpoints { jwt, ld, statement })
-                .map_err(|e| e.to_string())?,
+            client: Arc::new(
+                RebaseClient::new(Endpoints { jwt, ld, statement }).map_err(|e| e.to_string())?,
+            ),
         })
     }
-}
 
-#[wasm_bindgen]
-pub async fn statement(client: Client, req: String) -> Promise {
-    future_to_promise(async move {
-        let req: StatementReq = jserr!(serde_json::from_str(&req));
-        let res = jserr!(client.client.statement(req).await);
-        Ok(jserr!(serde_json::to_string(&res)).into())
-    })
-}
+    #[wasm_bindgen]
+    pub fn statement(&self, req: String) -> Promise {
+        let client = self.client.clone();
+        future_to_promise(async move {
+            let req: StatementReq = jserr!(serde_json::from_str(&req));
+            let res = jserr!(client.statement(req).await);
+            Ok(jserr!(serde_json::to_string(&res)).into())
+        })
+    }
 
-#[wasm_bindgen]
-pub async fn jwt(client: Client, req: String) -> Promise {
-    future_to_promise(async move {
-        let req: WitnessReq = jserr!(serde_json::from_str(&req));
-        let res = jserr!(client.client.jwt(req).await);
-        Ok(jserr!(serde_json::to_string(&res)).into())
-    })
-}
+    #[wasm_bindgen]
+    pub fn jwt(&self, req: String) -> Promise {
+        let client = self.client.clone();
+        future_to_promise(async move {
+            let req: WitnessReq = jserr!(serde_json::from_str(&req));
+            let res = jserr!(client.jwt(req).await);
+            Ok(jserr!(serde_json::to_string(&res)).into())
+        })
+    }
 
-#[wasm_bindgen]
-pub async fn ld(client: Client, req: String) -> Promise {
-    future_to_promise(async move {
-        let req: WitnessReq = jserr!(serde_json::from_str(&req));
-        let res = jserr!(client.client.ld(req).await);
-        Ok(jserr!(serde_json::to_string(&res)).into())
-    })
+    #[wasm_bindgen]
+    pub fn ld(&self, req: String) -> Promise {
+        let client = self.client.clone();
+        future_to_promise(async move {
+            let req: WitnessReq = jserr!(serde_json::from_str(&req));
+            let res = jserr!(client.ld(req).await);
+            Ok(jserr!(serde_json::to_string(&res)).into())
+        })
+    }
 }
