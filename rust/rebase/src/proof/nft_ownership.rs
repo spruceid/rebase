225197f57762c00bad26e15e085de3239833dbe7
K Rhoda <kelsey.rhoda@spruceid.com>
2023-03-20 09:41:16 -0600

    Changes witness to have use both public and private config
    Corrects typos, adds comments, improves naming consistency

--- rust/rebase/src/proof/nft_ownership.rs
+++ rust/rebase/src/proof/nft_ownership.rs
@@ -1,6 +1,6 @@
 use crate::{
     content::nft_ownership::NftOwnership as Ctnt,
-    statement::nft_ownership::AlchemyStatement as Stmt,
+    statement::nft_ownership::NftOwnershipStatement as Stmt,
     types::{
         defs::{Proof, Statement},
         error::{ProofError, StatementError},
@@ -11,18 +11,18 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Deserialize, JsonSchema, Serialize)]
 #[serde(rename = "proof")]
-pub struct AlchemyProof {
+pub struct NftOwnershipProof {
     pub signature: String,
     pub statement: Stmt,
 }
 
-impl Statement for AlchemyProof {
+impl Statement for NftOwnershipProof {
     fn generate_statement(&self) -> Result<String, StatementError> {
         self.statement.generate_statement()
     }
 }
 
-impl Proof<Ctnt> for AlchemyProof {
+impl Proof<Ctnt> for NftOwnershipProof {
     fn to_content(&self, statement: &str, signature: &str) -> Result<Ctnt, ProofError> {
         Ok(Ctnt {
             contract_address: self.statement.contract_address.clone(),
--- rust/rebase/src/statement/poap_ownership.rs
+++ rust/rebase/src/statement/poap_ownership.rs
@@ -10,6 +10,14 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Deserialize, JsonSchema, Serialize)]
 #[serde(rename = "statement")]
 pub struct PoapOwnership {
+    // NOTE: This should ideally be a u64
+    // i64 is being used because it comes out
+    // correctly in the JSON Schema
+    // The Rust JSON Schema lib adds a marker
+    // for the u64 that the JS lib cannot understand
+    // There is likely a clean way to use u64, but
+    // the only consequence of a negative event id
+    // is not finding anything on look up.
     pub event_id: i64,
     pub issued_at: String,
     pub subject: Subjects,
